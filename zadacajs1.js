b91435c97e97922ea09f20d49a1cba2af5f13fb6
zdravko12 <zdravkoena@yahoo.com>
2023-02-15 16:14:29 +0100

    HomeWorkLecture10

---
+++ zadacajs1.js
@@ -0,0 +1,86 @@
+let name1 = 1;
+let name2 = 0;
+let name3 = "1";
+let name4 = "0";
+let name5 = "ten";
+let name6 = true;
+let name7 = false;
+let name8 = null;
+let name9 = undefined;
+let name10 = "";
+let name11 = '';
+// bollean  6 i 7
+name8 = String("name8");     
+console.log("name8");
+
+name9 = String("name9");     
+console.log("name9");
+
+name8 = Number("name8");     
+console.log("name8");
+
+name9 = Number("name9");     
+console.log("name9");
+
+// string  1 2 6 7 10 11
+name1 = Boolean("name1");     
+console.log("name1");
+
+name2 = Boolean("name2");     
+console.log("name2");
+
+name6 = Boolean("name6");     
+console.log("name6")
+name7 = Boolean("name7");     
+console.log("name7");
+
+name10 = Boolean("name10");     
+console.log("name10");
+
+name11 = Boolean("name11");     
+console.log("name11");
+
+
+// number <-
+name1 = Number("name1");     
+console.log("name1");
+
+name2 = Number("name2");     
+console.log("name2");
+
+name6 = Number("name6");     
+console.log("name6")
+
+name7 = Number("name7");     
+console.log("name7");
+name10 =  Number("name10");     
+console.log("name10");
+
+name11 =  Number("name11");     
+console.log("name11");
+
+// Number za 3 4 
+name3 = String("name3");     
+console.log("name3");
+
+name4 = String("name4");     
+console.log("name4");
+// Boolean<-
+name3 = Boolean("name3");     
+console.log("name3")
+
+name4  = Boolean("name4 ");     
+console.log("name4 ");
+
+// ndefined/Null 8 9 string
+name8 = String("name8");     
+console.log("name8");
+
+name9  = String("name9 ");     
+console.log("name9");
+
+
+
+
+
+
---
+++ zadacajs2.js
@@ -0,0 +1,40 @@
+let birthDay = 24;
+let fullName = 16;
+let birthYear = 74;
+let year = 1+9+9+3;
+// za godina na rod
+let birthDay1 = 1993;
+let x = 19;
+let y = 93;
+let z = (19-93);
+console.log(z);
+
+let frist1 = birthDay > fullName;
+
+
+// godina
+let year2 = 1993;
+let sum = (1+9+9+3);
+console.log(sum);
+
+
+// true or false
+if(birthDay > fullName ){
+    console.log("true");
+}else{
+    console.log("false");
+}
+
+
+// pr0veruvam sumata na ostatokot
+let suma = birthDay1 %  year2; 
+console.log(suma);
+
+let vari7 = "I am the" 
+
+if(frist1 === true && suma < 3){
+    console.log("I am the light");
+}else{
+    console.log("I am the darkness");
+}
+
